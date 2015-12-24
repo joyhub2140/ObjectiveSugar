ce5fb5d299152e7cfbf00a98f77cb55c09b02113
admin <admin@admindeiMac-9.local>
2015-12-24 15:59:10 +0800

    测试提交第二个按钮

--- Example/ObjectiveSugar/AppDelegate.m
+++ Example/ObjectiveSugar/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     NSLog(@"fdafdafdafadsfds");
+    NSLog(@"测试第二个按钮");
     return YES;
 }
 
